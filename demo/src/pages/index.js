56e4f46c429235614c2c4bed0420610a606be444
lbaker <lbaker@newrelic.com>
2022-05-05 16:23:19 -0700

    chore: Add more content to demo page

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -16,6 +16,7 @@ import {
   SimpleFeedback,
   Surface,
   Table,
+  TableOfContents,
   Tag,
   TagList,
   Terminal,
@@ -144,7 +145,28 @@ const IndexPage = () => {
           />
         </section>
         <section>
-          <h2>Collapsers</h2>
+          <h2 id="tbc">Table of contents</h2>
+          <TableOfContents
+            headings={[
+              { id: 'tbc', text: 'Table of contents' },
+              { id: 'coll', text: 'Collapsers' },
+            ]}
+          />
+        </section>
+        <section>
+          <h2>Text styling</h2>
+          <br />
+          <blockquote>
+            Hello I'm a blockquote <br /> I've got plenty to say
+          </blockquote>
+          <ul>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+          </ul>
+        </section>
+        <section>
+          <h2 id="coll">Collapsers</h2>
           <CollapserGroup>
             <Collapser id="example-1" title="Check out this cool collapser">
               This is a pretty neat little utility. I can show all kinds of
@@ -155,6 +177,7 @@ const IndexPage = () => {
             </Collapser>
             <Collapser title="This one is open by default" defaultOpen>
               And you can see everything inside of it!
+              <code>hello here is some code woohoo</code>
             </Collapser>
           </CollapserGroup>
         </section>
@@ -357,7 +380,7 @@ nr1 create --type nerdpack --name pageviews-app
               border-radius: 4px;
               background: var(--primary-hover-color);
               .dark-mode & {
-                background: var(--primary-contrast-color);
+                background: var(--system-background-surface-1-dark);
               }
             `}
           >
